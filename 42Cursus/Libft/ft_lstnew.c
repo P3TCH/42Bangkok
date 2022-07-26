279c3f4f0f6bc7e0ee4da782e91a34730d33df6a
Petch User <petchuser@outlook.com>
2022-07-27 03:33:35 +0700

    new

---
+++ 42Cursus/Libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snamesst <snamesst@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 02:42:40 by snamesst          #+#    #+#             */
+/*   Updated: 2022/07/27 02:42:40 by snamesst         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (new == 0)
+		return (0);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
--- 42Cursus/Libft/libft.h
+++ 42Cursus/Libft/libft.h
@@ -14,8 +14,14 @@
 # define LIBFT_H
 
 # include <stdlib.h>
-# include <string.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -52,4 +58,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **list, t_list *new);
 #endif
